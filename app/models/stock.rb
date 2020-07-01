0380459b03b64f9904a69c61418459ba01d9e153
Nirmal <nirmalkumar91@gmail.com>
2020-07-01 18:17:23 +0530

    minor change 4

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -17,8 +17,5 @@ class Stock < ApplicationRecord
         where(ticker: ticker_symbol).first
     end
 
-    def self.get_all_stocks
-        client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_key], endpoint: 'https://sandbox.iexapis.com/v1')
-        client.stock_market_list(:mostactive)
-    end
+    
 end
