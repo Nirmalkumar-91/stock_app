e670fe90e1d910bad6064ce23cd0eb7782be6739
Nirmal <nirmalkumar91@gmail.com>
2020-07-01 13:25:01 +0530

    Finalized Stock app

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
-  root 'welcome#index'
+  root 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'friendships#my_friends'
   get 'search_friends', to: 'friendships#search'
   resources :friendships, only: [:destroy, :create]
+  resources :users, only: [:show]
 end
