57f44d6e32b5e667a18fe66bde39a2fa07e62626
Nirmal <nirmalkumar91@gmail.com>
2020-07-01 17:38:03 +0530

    minnor change 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
-  root 'welcome#index'
+  root to: 'welcome#index'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'friendships#my_friends'
   get 'search_friends', to: 'friendships#search'
