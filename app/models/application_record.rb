fa654ea6991ae38b09f335fed0da381bb48627e5
Nirmal <nirmalkumar91@gmail.com>
2020-07-01 17:48:15 +0530

    minnor changes 2

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  def self.get_all_stocks
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_key], endpoint: 'https://sandbox.iexapis.com/v1')
+    client.stock_market_list(:mostactive)
+  end
 end
